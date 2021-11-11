bffaf4fd43d8b0283222c45039608c0f58d96d36
Burton <c1023977@hallam.shu.ac.uk>
2021-11-11 10:00:04 +0000

    added on load

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,13 @@
-document.querySelector(".red").addEventListener("click", function(ev) {
-  document.querySelector("body").setAttribute("class", "redBack");
-});
+function init() {
+  document.querySelector(".red").addEventListener("click", function(ev) {
+    document.querySelector("body").setAttribute("class", "redBack");
+  });
 
-document.querySelector(".green").addEventListener("click", function(ev) {
-  document.querySelector("body").setAttribute("class", "greenBack");
-});
+  document.querySelector(".green").addEventListener("click", function(ev) {
+    document.querySelector("body").setAttribute("class", "greenBack");
+  });
 
-document.querySelector(".blue").addEventListener("click", function(ev) {
-  document.querySelector("body").setAttribute("class", "blueBack");
-});
+  document.querySelector(".blue").addEventListener("click", function(ev) {
+    document.querySelector("body").setAttribute("class", "blueBack");
+  });
+}
