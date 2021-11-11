940f647ddf832fa98c82445ed989f9c20a5f62b8
Burton <c1023977@hallam.shu.ac.uk>
2021-11-11 09:43:27 +0000

    added colour changing buttons

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,11 @@
-alert("hello");
+document.querySelector(".red").addEventListener("click", function(ev) {
+  document.querySelector("body").setAttribute("class", "redBack");
+});
+
+document.querySelector(".green").addEventListener("click", function(ev) {
+  document.querySelector("body").setAttribute("class", "greenBack");
+});
+
+document.querySelector(".blue").addEventListener("click", function(ev) {
+  document.querySelector("body").setAttribute("class", "blueBack");
+});
