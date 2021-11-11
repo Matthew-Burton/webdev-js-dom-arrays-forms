714e68e27c00df126a64dee546b877110c7275fb
Burton <c1023977@hallam.shu.ac.uk>
2021-11-11 10:24:09 +0000

    added image js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -20,4 +20,18 @@ function init() {
   document.querySelector(".blue").addEventListener("click", function(ev) {
     document.querySelector("body").setAttribute("class", "blueBack");
   });
+
+  var imageAr = [
+    "images/view1.jpg",
+    "images/view2.jpg",
+    "images/view3.jpg",
+    "images/view4.jpg",
+    "images/view5.jpg",
+    "images/view6.jpg"
+  ];
+
+  function chgImage() {
+    document.getElementById("myImages").setAttribute("src", imageAr[3]);
+  }
+  chgImage();
 }
